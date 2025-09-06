e92a47bf8d617c9f8be1c4f0d2f9d945ad03695d
Tomoya Kamimura <tenjinsan@gmail.com>
2025-09-06 09:16:05 +0900

    pythonプログラムをwhileループに変更

--- spirngmass_python.py
+++ spirngmass_python.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
-import matplotlib.pyplot as plt
+import numpy as np # 数値計算ライブラリの読み込み
+import matplotlib.pyplot as plt # 描画ライブラリの読み込み
 
 
 def eom(y):
@@ -11,27 +11,25 @@ def eom(y):
     dy = np.array([v, (-k*x - c*v)/m])
     return dy
 
-def Eular(y0, dt, tspan):
+def Eular(y0, dt, tend):
     # オイラー法
-    n = (tspan[1]-tspan[0])/dt
-    t = tspan[0]
+    t = 0.0
     y = np.copy(y0) # y=y0だと、同一アドレスが共有されてしまう
     tout = np.copy(t)
     yout = np.copy(y)
-    for i in range(1,n.astype(int)):
+    while t < tend:
         t += dt
         y += eom(y)*dt
         tout = np.vstack((tout, t)) # 結果を積み上げる
         yout = np.vstack((yout, y)) # 結果を積み上げる
     return tout, yout
 
-def RK4(y0, dt, tspan):
-    n = (tspan[1]-tspan[0])/dt
-    t = tspan[0]
+def RK4(y0, dt, tend):
+    t = 0.0
     y = np.copy(y0) # y=y0だと、同一アドレスが共有されてしまう
     tout = np.copy(t)
     yout = np.copy(y)
-    for i in range(1,n.astype(int)):
+    while t < tend:
         t += dt
         k1 = eom(y)
         k2 = eom(y + 0.5*k1*dt)
@@ -50,11 +48,11 @@ c = 10.0
 # 初期値、刻み幅、シミュレーション時間の定義
 y0 = np.array([1.0, 0.0])
 dt = 1e-2
-tspan = np.array([0.0, 3.0])
+tend = 3.0
 
 # 数値積分の実行
-# tout, yout = Eular(y0,dt,tspan)
-tout, yout = RK4(y0, dt, tspan)
+# tout, yout = Eular(y0,dt,tend)
+tout, yout = RK4(y0, dt, tend)
 
 # 結果の描画
 plt.figure()
